eb73f208b4de4c876e8e48289b7f6af063f6c15e
Mika Saari <mika.saari@tuni.fi>
2023-03-23 10:25:19 +0200

    generalization of exceptions

--- Sisu/src/main/java/fi/tuni/prog3/sisu/iReadAndWriteToFile.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/iReadAndWriteToFile.java
@@ -15,15 +15,15 @@ public interface iReadAndWriteToFile {
      * Reads JSON from the given file.
      * @param fileName name of the file to read from.
      * @return true if the read was successful, otherwise false.
-     * @throws FileNotFoundException if the file cannot be accessed. 
+     * @throws Exception if the method e.g, cannot find the file. 
      */
-    boolean readFromFile(String fileName) throws FileNotFoundException; 
+    boolean readFromFile(String fileName) throws Exception; 
     
     /**
      * Write the student progress as JSON into the given file.
      * @param fileName name of the file to write to.
      * @return true if the write was successful, otherwise false.
-     * @throws IOException if it cannot write to a file.
+     * @throws Exception if the method e.g., cannot write to a file.
      */
-    boolean writeToFile(String fileName) throws IOException;
+    boolean writeToFile(String fileName) throws Exception;
 }
