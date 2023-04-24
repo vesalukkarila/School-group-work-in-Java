799300b7ba6d47c052d20cdb6e1cee2f373a8e31
Mikko Juntunen <mikko.juntunen@tuni.fi>
2023-04-24 14:39:19 +0300

    Init new class RootSearch.

---
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/RootSearch.java
@@ -0,0 +1,20 @@
+package fi.tuni.prog3.sisu;
+
+/**
+ * This interface handles the rootsearch to Sisu API
+ * returns all degrees in 2023
+ * @author vesalukkarila, mikkojuntunen
+ */
+public interface RootSearch {
+    
+    
+    /**
+     * Makes a query to Sisu API of all the degreeprogrammes in 2023
+     * and with the help of handleAllDegreeModules, creates objects of these
+     * degreeprogrammes and adds them to an arraylist
+     * @return Arraylist full of degreeprogramme-objects
+     * @throws MalformedURLException
+     * @throws IOException 
+     */
+
+}
