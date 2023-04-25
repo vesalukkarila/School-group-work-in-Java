b11a4f74522d660942bb3c21d3349c2b91588b3b
Mikko Juntunen <mikko.juntunen@tuni.fi>
2023-04-25 13:48:30 +0300

    Add class StudyModule.

---
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/StudyModule.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.prog3.sisu;
+
+/**
+ *
+ * @author vesalukkarila
+ */
+public class StudyModule extends DegreeModule{
+    
+    public StudyModule(String name, String id, String groupId, int minCredits, String moduleType) {
+        super(name, id, groupId, minCredits, moduleType);
+    }
+}
