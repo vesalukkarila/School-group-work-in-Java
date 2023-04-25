792a142be3e0b3d9c34e4382499b52754fb0205f
Mikko Juntunen <mikko.juntunen@tuni.fi>
2023-04-25 13:47:58 +0300

    Add class GroupingModule.

---
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/GroupingModule.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.prog3.sisu;
+
+/**
+ *
+ * @author vesalukkarila
+ */
+public class GroupingModule extends DegreeModule{
+    
+    public GroupingModule(String name, String id, String groupId, int minCredits, String moduleType) {
+        super(name, id, groupId, minCredits, moduleType);
+    }
+}
