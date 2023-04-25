d47c867bf7e5b094732d1d35d68e98a9a219b84e
Mikko Juntunen <mikko.juntunen@tuni.fi>
2023-04-25 13:47:23 +0300

    Add class ActualDegreeModule.

---
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/ActualDegreeModule.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.prog3.sisu;
+
+/**
+ *
+ * @author vesalukkarila
+ */
+public class ActualDegreeModule extends DegreeModule {
+    
+    public ActualDegreeModule(String name, String id, String groupId, int minCredits, String moduleType) {
+        super(name, id, groupId, minCredits, moduleType);                                      
+        
+    } 
+}
