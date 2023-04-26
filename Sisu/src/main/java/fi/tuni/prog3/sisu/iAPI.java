47e420ead84a4d803f2a3e27129666ec6a35af90
Vesa Lukkarila <github@vesalukkarila.com>
2023-04-26 09:49:17 +0300

    add first half of depth-first-algorithm

--- Sisu/src/main/java/fi/tuni/prog3/sisu/iAPI.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/iAPI.java
@@ -22,7 +22,63 @@ public interface iAPI {
     
     public static DegreeModule findModules (URL url) throws IOException {
         
-       
+        ArrayList <String> typesArrayList = new ArrayList<>();
+        typesArrayList.add("CourseUnitRule");
+        typesArrayList.add("ModuleRule");
+        
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setConnectTimeout(5000);
+        con.setReadTimeout(5000);
+        
+        BufferedReader textIn = new BufferedReader(
+            new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = textIn.readLine()) != null) {
+            content.append(inputLine);
+        }
+        textIn.close();
+        
+        String mjono = content.toString();
+        JsonElement fileElement = JsonParser.parseString(mjono);
+        JsonObject fileObject = null;
+        
+        if (fileElement.isJsonArray()) {
+            JsonArray array = fileElement.getAsJsonArray();
+            for (JsonElement element : array) {
+                fileObject = element.getAsJsonObject();         
+            }
+        }
+        else {
+            fileObject = fileElement.getAsJsonObject();
+        }
+        
+        DegreeModule moduleObject = null; 
+        
+        //Identifying and creating object accordingly
+        if (fileObject.has("courseUnitType")) {
+            return handleCourse(fileObject);
+        }
+        
+        
+        else {
+            String objectType = fileObject.get("type").getAsString();
+            switch (objectType) {
+                case "GroupingModule" ->  moduleObject = handleGroupingModule(fileObject);                   
+                case "DegreeProgramme" -> moduleObject = handleDegreeModule(fileObject);                    
+                case "StudyModule" -> moduleObject = handleStudyModule(fileObject);                    
+                default -> {
+                }
+            }
+        }
+     
+        
+        
+        
+        
+        
     }
     
     
