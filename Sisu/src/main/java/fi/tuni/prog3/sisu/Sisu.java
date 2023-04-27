a48ad784bc9ac76c0846342190d55766ab7ab8bd
Vesa Lukkarila <github@vesalukkarila.com>
2023-04-27 11:05:02 +0300

    add new gui

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Sisu.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Sisu.java
@@ -1,18 +1,28 @@
 package fi.tuni.prog3.sisu;
 
 import java.io.IOException;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -25,82 +35,97 @@ public class Sisu extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         
-        //Creating a new BorderPane.
         BorderPane root = new BorderPane();
-        root.setPadding(new Insets(10, 10, 10, 10));
+        TabPane tabPane = new TabPane();
+        ChoiceBox choiceBox = new ChoiceBox<>();                    //<STRING>
         
-        //Adding HBox to the center of the BorderPane.
-        root.setCenter(getCenterHbox());
-        
-        //Adding button to the BorderPane and aligning it to the right.
-        var quitButton = getQuitButton();
-        BorderPane.setMargin(quitButton, new Insets(10, 10, 0, 10));
-        root.setBottom(quitButton);
-        BorderPane.setAlignment(quitButton, Pos.TOP_RIGHT);
-        
-        Scene scene = new Scene(root, 800, 500);                      
-        stage.setScene(scene);
-        stage.setTitle("SisuGUI");
-        
-        
-        VBox left = (VBox) root.lookup("#leftbox");
-        VBox right = (VBox) root.lookup("#rightbox");
-        Label infoLabel = (Label) root.lookup("#infoLabel");
-        Button chooseButton = (Button) root.lookup("#chooseButton");
-        
-        ChoiceBox choiceBox = new ChoiceBox();
-        choiceBox.setId("choicebox");
-        left.getChildren().add(choiceBox);
-        
-        
-        //Fetching all degreeprogrammes from Sisu-api
-        ArrayList<ActualDegreeModule> degreeList = RootSearch.allDegreeProgrammes();       
-        
+        ArrayList<ActualDegreeModule> degreeList = RootSearch.allDegreeProgrammes();
         
         for (var degmod : degreeList) {                                            
             choiceBox.getItems().add(degmod.getName());
         }
         
-        chooseButton.setOnAction(new EventHandler<ActionEvent>() {
+        //Left tab
+        Tab tabLeft = new Tab("Choose a Degree Programme");
+        tabLeft.setClosable(false);
+        Button button1 = getQuitButton();
+        VBox tab1Content = new VBox(new Label("Choose an degree:"), choiceBox, button1);
+        
+        
+        tab1Content.setSpacing(10);
+        VBox.setVgrow(tab1Content, Priority.ALWAYS);
+        HBox bottomBox1 = new HBox(button1);
+        bottomBox1.setAlignment(Pos.CENTER_RIGHT);
+        bottomBox1.setPadding(new Insets(10));
+        VBox.setVgrow(bottomBox1, Priority.NEVER);
+        VBox vbox1 = new VBox(tab1Content, bottomBox1);
+        tabLeft.setContent(vbox1);
+        
+        //Right tab
+        Tab tabRight = new Tab("View chosen degree");
+        
+        tabRight.setClosable(false);
+        TreeView tree = new TreeView();
+        tree.setId("emptytree");
+        VBox tab2Content = new VBox(tree);
+        
+        tab2Content.setId("tab2content");
+        tab2Content.setSpacing(10);
+        VBox.setVgrow(tab2Content, Priority.ALWAYS);
+        HBox bottomBox2 = bottomBox1;              
+        bottomBox2.setAlignment(Pos.CENTER_RIGHT);
+        bottomBox2.setPadding(new Insets(10));
+        VBox.setVgrow(bottomBox2, Priority.NEVER);
+        VBox vbox2 = new VBox(tab2Content, bottomBox2);
+        tabRight.setContent(vbox2);
+        
+        
+        tabLeft.setStyle("-fx-background-color: rgb(45, 214, 169)");
+        tabRight.setStyle("-fx-background-color: rgb(100, 232, 197)");
+        button1.setStyle("-fx-background-color: rgb(237, 84, 74)");
+        
+        
+        tabPane.getTabs().addAll(tabLeft, tabRight);
+        root.setCenter(tabPane);
+        VBox bottomBox = new VBox(bottomBox1);
+        bottomBox.setAlignment(Pos.CENTER_RIGHT);
+        root.setBottom(bottomBox);
+        Scene scene = new Scene(root, 400, 400);
+        
+        choiceBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
                                 
-                VBox right = (VBox) root.lookup("#rightbox");
                 int selectedIndex = choiceBox.getSelectionModel().getSelectedIndex();
                 ActualDegreeModule module = degreeList.get(selectedIndex);
                
                 //Building url
-                String groupId = module.getGroupId();                           //tää tehtäis muualla
+                String groupId = module.getGroupId();                         
                 URL url = null;
                 
                 try {
                     url = getUrl(groupId);
                 } catch (MalformedURLException ex) {                              
-                    Logger.getLogger(Sisu.class.getName()).log(Level.SEVERE, null, ex);
+                    java.util.logging.Logger.getLogger(Sisu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
                 }
-                  
-                //Fetching all module-objects from Sisu API for the chosen degree
                 
                 try {
-                    DegreeModule degree = iAPI.findModules(url);                  //tää tehtäis muualla
-                    //Running through the degree-structure
-                    TreeItem rootItem = treeItemsRecursive(degree);         //tähän takas
-
-                    TreeView treeView = new TreeView();
-                    treeView.setRoot(rootItem);
-                    right.getChildren().clear();
-                    right.getChildren().add(treeView);
-                    String degreeString = (String) choiceBox.getValue();       
-                    infoLabel.setText("Showing: "+ degreeString); 
+                    //Fetching all module-objects from Sisu API for the chosen degree
+                    DegreeModule degree = iAPI.findModules(url);                 
                     
+                    //Running through the degree-structure
+                    TreeItem rootItem = treeItemsRecursive(degree);
+                    
+                    TreeView tree = (TreeView) root.lookup("#emptytree");
+                    tree.setRoot(rootItem);
                     
                 } catch (IOException ex) {
-                    Logger.getLogger(Sisu.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
+                    java.util.logging.Logger.getLogger(Sisu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                }   
             }
-        
         });
+        
+        stage.setScene(scene);
         
         stage.show();
     }
--- Sisu/src/main/java/module-info.java
+++ Sisu/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module fi.tuni.prog3.sisu {
     requires javafx.controls;
     exports fi.tuni.prog3.sisu;
     requires com.google.gson;
+    requires java.logging;
 }
